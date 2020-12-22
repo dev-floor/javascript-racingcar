ed8a8bfaf3ad94f90c78b3bd08fb39a832a9e470
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-12-22 17:06:35 +0900

    RacingGame 생성자를 통한 초기 세팅

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-export default function RacingCarGame() {
+import {hiddenInitView} from "./initialization.js";
+
+export default class RacingCarGame {
+ constructor() {
+   this.init();
+ }
+ 
+ // 초기 세팅 - view 관리 (보여질 것과 가릴 것)
+ init() {
+   // 횟수와 결과 view는 처음에는 안보이게 처리.
+  hiddenInitView();
+ }
 }
 
 new RacingCarGame();
