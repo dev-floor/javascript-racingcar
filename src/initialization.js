91a158f38f8589f37fb37d996aafee3b8136b86f
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-12-22 17:08:25 +0900

    초기 view 세팅 처리를 위한 모듈화

---
+++ src/initialization.js
@@ -0,0 +1,10 @@
+function hiddenInitView() {
+  const racingCountContainer = document.getElementById("counting-container");
+  const resultContainer = document.getElementById("result-container");
+
+  racingCountContainer.style.display = 'none';
+  resultContainer.style.display = 'none';
+}
+
+
+export {hiddenInitView};
