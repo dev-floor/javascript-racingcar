f354417665948bfe01f953213668a28b74a8f1c6
JeongPyo <jeongpyo_park@tmax.co.kr>
2020-12-23 15:24:42 +0900

    자동차 입력 받기 및 조건 검사 기능 구현

--- src/inputCarName.js
+++ src/inputCarName.js
@@ -15,9 +15,25 @@ function inputCarName() {
 
   function validationCarName() {
     // input tag 값에 대한 유효성 체크 
-    // 이상 있을 때 alert 경고.
-    // 정상이라면
-    showView();
+    let inputStr = carsNameInput.value;
+    let inputSplit = inputStr.split(',');
+    let validationFlagValue = true;
+
+    // 길이 체크
+    for(let i in inputSplit) {
+      console.log(inputSplit[i].length)
+      if(inputSplit[i].length > 5) {
+        alert('자동차 이름의 길이를 5자 이하로 해주세요.');
+        validationFlagValue = false;
+        break;
+      }
+    }
+    
+    // name 정상 입력시 하단 view 보여주기
+    if(validationFlagValue) {
+      showView();
+    }
+     
   }
 }
 
